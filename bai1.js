f50fc41cc644babb75b8b8fefccce07e327f3a1d
minh chau <chaugriezmann963@gmail.com>
2023-09-14 12:40:42 +0700

    first

---
+++ bai1.js
@@ -0,0 +1,9 @@
+let year;
+year = prompt("nhập năm");
+let month;
+month = prompt("nhập tháng");
+let days;
+days = prompt("nhập ngày");
+let total;
+total = days+"/" + month+"/" + year;
+document.write(total)
